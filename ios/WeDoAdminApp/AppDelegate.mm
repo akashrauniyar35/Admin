967a0d30c13f55271b3f7257c48989132c32405b
Akash Rouniyar <akashrouniyar@192-168-1-114.tpgi.com.au>
2023-03-09 19:14:59 +1100

    fixings

--- ios/WeDoAdminApp/AppDelegate.mm
+++ ios/WeDoAdminApp/AppDelegate.mm
@@ -51,7 +51,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 #endif
 
   NSDictionary *initProps = [self prepareInitialProps];
-  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"Admin", initProps);
+  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"WeDo Admin App", initProps);
 
   if (@available(iOS 13.0, *)) {
     rootView.backgroundColor = [UIColor systemBackgroundColor];
